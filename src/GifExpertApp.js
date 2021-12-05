b710321943244943d2850c21153262b9d5354952
Gabriel C <gchaparro@arbusta.net>
2021-12-04 23:19:08 -0300

    app terminada

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-    return (
-        <>
-        <h2> GifExpertApp</h2>
-        <hr />
-        </>
-    )
-}
+  /*  const categories = ['One Punch', 'Samurai X', 'Dragon Ball']; */
+
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  /* const handleAdd = () => {
+    setCategories( cats => [...cats, "HunterXHunter"]); //al final
+
+   //setCategories(["HunterXHunter", ...categories]); //al principio
+    console.log(categories);
+  }; */
+
+
+  return (
+    <>
+      <h2> GifExpertApp</h2>
+      <AddCategory setCategories={ setCategories }/>
+      <hr />
+      
+      <ol>
+        {
+            categories.map((category) => (
+            <GifGrid 
+                 key= {category}
+                 category={category} />
+        
+                 ))
+        }
+      </ol>
+    </>
+  );
+};
